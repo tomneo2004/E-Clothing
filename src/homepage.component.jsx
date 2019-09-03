a4d83c28547e8edc1021a12b73fc1c4efb5cd061
Nelson <tomneo2004@gmail.com>
2019-09-03 14:23:27 +0800

    layout homepage and add sass

---
+++ src/homepage.component.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+const homepage = ()=>{
+
+    return (
+
+        <div className='homepage'>
+            <div className='directory-menu'>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>Hat</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>Jacket</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>Sneakers</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>Womens</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>Mens</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default homepage;

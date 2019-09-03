0e13c421141f201aada1334590396f4e9519a243
Nelson <tomneo2004@gmail.com>
2019-09-03 17:29:48 +0800

    Add routing to hats

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import {Route, Switch} from 'react-router-dom';
 import HomePage from './Pages/HomePage/homepage.component';
+import HatsPage from './Pages/HatsPage/hats.component';
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/shop/hats' component={HatsPage} />
+    </Switch>
   );
 }
 
--- src/Components/directory/directory.component.jsx
+++ src/Components/directory/directory.component.jsx
@@ -51,8 +51,8 @@ class Directory extends Component{
             <div className='directory-menu'>
 
                 {
-                    this.state.sections.map(({title, id, imageUrl, size})=>{
-                        return <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                    this.state.sections.map(({id, ...otherSectionProps})=>{
+                        return <MenuItem key={id} {...otherSectionProps}/>
                     })
                 }
             </div>

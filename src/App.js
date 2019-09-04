c31a4f825e776ac4924e9cc92d85489944b9a684
Nelson <tomneo2004@gmail.com>
2019-09-04 09:30:25 +0800

    Add header, add logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import {Route, Switch} from 'react-router-dom';
+import Header from './Components/header/header.component';
 import HomePage from './Pages/HomePage/homepage.component';
 import ShopPage from './Pages/ShopPage/shop.component';
 
@@ -8,10 +9,14 @@ import ShopPage from './Pages/ShopPage/shop.component';
 function App() {
   return (
 
-    <Switch>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/shop' component={ShopPage} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
+
   );
 }
 

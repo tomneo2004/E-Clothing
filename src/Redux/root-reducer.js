d96dd7fbcc96860a211747a6b500008425868881
Nelson <tomneo2004@gmail.com>
2019-09-09 11:00:31 +0800

    Add Add to cart button

--- src/Redux/root-reducer.js
+++ src/Redux/root-reducer.js
@@ -1,9 +1,9 @@
 import {combineReducers} from 'redux';
 import UserReducer from './user/user.reducer';
 import CartReducer from './cart/cart.reducer';
-import cartReducer from './cart/cart.reducer';
+
 
 export default combineReducers({
     user:UserReducer,
-    cart:cartReducer
+    cart:CartReducer
 });

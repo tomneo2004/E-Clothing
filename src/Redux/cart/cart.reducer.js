ed8d6ac0eaa9cd9acc26df54ae536b8b7fbb8c4d
Nelson <tomneo2004@gmail.com>
2019-09-09 11:53:33 +0800

    Add item to cart and group items

--- src/Redux/cart/cart.reducer.js
+++ src/Redux/cart/cart.reducer.js
@@ -1,13 +1,17 @@
 import CartActionTypes from './cart.types';
+import {addItemToCart} from './cart.util';
 
 const initState = {
-    cartHidden: true
+    cartHidden: true,
+    cartItems:[]
 }
 const cartReducer = (state = initState, action)=>{
 
     switch(action.type){
         case CartActionTypes.TOGGLE_CART_HIDDEN:
             return {...state, cartHidden:!state.cartHidden};
+        case CartActionTypes.ADD_ITEM:
+            return {...state, cartItems:addItemToCart(state.cartItems, action.payload)};
 
         default:
             return state; 
---
+++ src/Redux/cart/cart.util.js
@@ -0,0 +1,13 @@
+export const addItemToCart = (cartItems, itemToAdd)=>{
+
+    const existingItem = cartItems.find((item)=>(item.id===itemToAdd.id));
+
+    if(existingItem){
+
+        return cartItems.map(item=>{
+            return item.id===itemToAdd.id?{...item, quantity:item.quantity+1}:item
+        });
+    }
+
+    return [...cartItems, {...itemToAdd, quantity:1}];
+}

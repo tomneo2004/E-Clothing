b21bff385a55d85b5eda387570a4c5523676d564
Nelson <tomneo2004@gmail.com>
2019-09-10 14:24:51 +0800

    Add redux-persist to persist cart item

--- src/Redux/root-reducer.js
+++ src/Redux/root-reducer.js
@@ -1,9 +1,18 @@
 import {combineReducers} from 'redux';
 import UserReducer from './user/user.reducer';
 import CartReducer from './cart/cart.reducer';
+import {persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
+const persistConfig = {
+    key:'root',
+    storage,
+    whitelist:['cart']
+}
 
-export default combineReducers({
+const rootReducer = combineReducers({
     user:UserReducer,
     cart:CartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,8 +1,10 @@
 import {createStore, applyMiddleware} from 'redux';
 import Logger from 'redux-logger';
 import RootReducer from './root-reducer';
+import {persistStore} from 'redux-persist';
 
 const middlewares = [Logger];
 const store = createStore(RootReducer, applyMiddleware(...middlewares));
+const persistor = persistStore(store);
 
-export default store;
+export {store, persistor};

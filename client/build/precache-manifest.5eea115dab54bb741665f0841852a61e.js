28db8a595ea67cf5f6e04e7a3a89eaced937dc7a
Nelson <tomneo2004@gmail.com>
2020-02-28 08:11:34 +0800

    gh-pages

---
+++ client/build/precache-manifest.5eea115dab54bb741665f0841852a61e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a814b041882dae3c30b1bf7d80ade0f4",
+    "url": "/E-Clothing/index.html"
+  },
+  {
+    "revision": "f9a3f0f74fdacb56c14f",
+    "url": "/E-Clothing/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d76e0625862c39394120",
+    "url": "/E-Clothing/static/js/2.eb2e57cd.chunk.js"
+  },
+  {
+    "revision": "f0e45ef87c3c36359f7d",
+    "url": "/E-Clothing/static/js/3.f04df51e.chunk.js"
+  },
+  {
+    "revision": "4fcbee8e9a3e75e36019",
+    "url": "/E-Clothing/static/js/4.a3537a20.chunk.js"
+  },
+  {
+    "revision": "efcfae3084d06439bca3",
+    "url": "/E-Clothing/static/js/5.e431391e.chunk.js"
+  },
+  {
+    "revision": "7c19fa1c02a8be5c30ae",
+    "url": "/E-Clothing/static/js/6.5780c9d8.chunk.js"
+  },
+  {
+    "revision": "0c94d58f7779eda0bd34",
+    "url": "/E-Clothing/static/js/7.8a773937.chunk.js"
+  },
+  {
+    "revision": "c659773e49e6eb00e223",
+    "url": "/E-Clothing/static/js/8.df3cda3f.chunk.js"
+  },
+  {
+    "revision": "db59e64c72022ecf52ca",
+    "url": "/E-Clothing/static/js/9.b0a0e79b.chunk.js"
+  },
+  {
+    "revision": "f9a3f0f74fdacb56c14f",
+    "url": "/E-Clothing/static/js/main.74fd8955.chunk.js"
+  },
+  {
+    "revision": "ba088e6278243dd58f4f",
+    "url": "/E-Clothing/static/js/runtime~main.7c854da6.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/E-Clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/E-Clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
